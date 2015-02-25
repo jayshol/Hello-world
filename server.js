4d22980b4fe93986aec94a31f480cd59c0e9d93b
Jayshol <jayshol@gmail.com>
2015-02-25 14:29:08 -0600

    Add tests

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var http = require('http');
-var server = http.createServer(function(req, res){
-	res.write("Hello world, its jai!");
+var server = http.createServer(function(req, res){	
+	res.write("Hello world, its jaisree!");
 	res.end();
 });
 
-var port = process.env.PORT || 9001;
+//var port = process.env.PORT || 9001;
 
-server.listen(port, function(){
-	console.log('listening at port %d', port);
+server.listen(3000, function(){
+	console.log('listening at port %d', 3000);
 });
